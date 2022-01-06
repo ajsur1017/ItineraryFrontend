56eb301bbc9a4e398e2ce161963b2716b11a9de0
Anthony <ajsur1017@gmail.com>
2022-01-06 14:08:12 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,193 @@
-import logo from './logo.svg';
-import './App.css';
+// Import our components
+import AllPosts from "./pages/AllPosts";
+import SinglePost from "./pages/SinglePost";
+import Form from "./pages/Form";
+import Auth from "./pages/Auth";
+import Dashboard from "./pages/Dashboard";
+import Nav from "./components/Nav";
 
-function App() {
+// import React Hooks
+import { useState, useEffect } from "react";
+
+// Import React Router Components
+import { Route, Switch, Link } from "react-router-dom";
+
+
+function App(props) {
+  ////////////
+  // Style Objects
+  ///////////
+
+  const h1 = {
+    textAlign: "center",
+    margin: "10px",
+  };
+
+  const button = {
+    backgroundColor: "navy",
+    display: "block",
+    margin: "auto"
+  }
+
+  //////////////////
+  // State & Other Variables
+  //////////////////
+
+  //api url
+  const url = "https://vast-gorge-85508.herokuapp.com/mondays/"
+  // const urlMonday = "https://vast-gorge-85508.herokuapp.com/mondays";
+  // const urlTuesday = "https://vast-gorge-85508.herokuapp.com/tuesdays";
+  // const urlWednesday = "https://vast-gorge-85508.herokuapp.com/wednesdays";
+  // const urlThursday = "https://vast-gorge-85508.herokuapp.com/thursdays";
+  // const urlFriday = "https://vast-gorge-85508.herokuapp.com/fridays";
+  // const urlSaturday = "https://vast-gorge-85508.herokuapp.com/saturdays";
+  // const urlSunday = "https://vast-gorge-85508.herokuapp.com/sundays"
+
+  // state to hold list of todos
+  const [posts, setPosts] = useState([]);
+
+  // an object that represents a null todo as a starting point
+  const nullTodo = {
+    name: "",
+    details: "",
+    time: ""
+  }
+
+  // const stat to hold todo for editing
+
+  const [targetTodo, setTargetTodo] = useState(nullTodo)
+
+  ///////////////
+  // Functions
+  ////////////////
+
+  const getTodos = async () => {
+    const response = await fetch(url)
+    const data = await response.json()
+    setPosts(data)
+  }
+
+  const addTodos = async (newTodo) => {
+    const response = await fetch(url, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTodo),
+    })
+    getTodos();
+  }
+
+  const getTargetTodo = (todo) => {
+    setTargetTodo(todo);
+    props.history.push("/edit");
+  }
+
+  const updateTodo = async (todo) => {
+    const response = await fetch(url + todo.id + "/", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(todo),
+    });
+
+    // get updated list of todos
+    getTodos();
+  }
+  // Function to edit todo on form submission
+  const deleteTodo = async (todo) => {
+    const response = await fetch(url + todo.id + "/", {
+      method: "delete",
+    });
+
+    // get updated list of todos
+    getTodos();
+    props.history.push("/");
+  };
+  ///////////////
+  // useEffects
+  ///////////////
+  // make the api call when the component
+  // loads only the first time
+  useEffect(() => {
+    getTodos()
+  }, [])
+
+  /////////////////
+  // Returned JSX
+  /////////////////
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={h1}>My Itinerary</h1>
+      <Link to="/new"><button style={button}>Create New Todo</button></Link>
+      <h2>Monday</h2>
+      <Switch>
+        {/* INDEX PAGE */}
+        <Route
+          exact path="/"
+          render={(rp) => {
+            return <AllPosts {...rp} posts={posts} />;
+          }}
+        />
+        {/* LOGIN PAGE */}
+        <Route
+          path="/auth/:form" 
+          component={Auth} />;
+
+        <Route
+          path="/dashboard"
+          component={Dashboard} />
+        {/* SHOW PAGE */}
+        <Route
+          path="/post/:id"
+          render={(rp) => {
+            return <SinglePost
+              {...rp}
+              posts={posts}
+              edit={getTargetTodo}
+              deleteTodo={deleteTodo}
+            />;
+          }}
+        />
+        {/* NEW AND EDIT FORM PAGES */}
+        <Route
+          path="/new"
+          render={(rp) => {
+            return <Form {...rp}
+              initialTodo={nullTodo}
+              handleSubmit={addTodos}
+              buttonLabel="Add to my list"
+            />;
+          }}
+        />
+        <Route
+          path="/edit"
+          render={(rp) => {
+            return <Form
+              {...rp}
+              initialTodo={targetTodo}
+              handleSubmit={updateTodo}
+              buttonLabel="Edit"
+            />;
+          }}
+        />
+      </Switch>
+      <h2>Tuesday</h2>
+
+      <h2>Wednesday</h2>
+
+      <h2>Thursday</h2>
+
+      <h2>Friday</h2>
+
+      <h2>Saturday</h2>
+
+      <h2>Sunday</h2>
     </div>
   );
 }
 
 export default App;
+
+// todos_ordered = Todo.where(…..).order(column_order: :asc)
---
+++ src/pages/SinglePost.js
@@ -0,0 +1,34 @@
+import React from "react"
+import {Link} from "react-router-dom"
+
+const SinglePost = ({posts, match, edit, deleteTodo}) => {
+
+    // grab the id from params
+    const id = parseInt(match.params.id)
+    // find the single post from the posts array
+    const post = posts.find((post) => post.id === id )
+
+    ///////////////////
+    // Style objects
+    ////////////////////
+    const div = {
+        textAlign:"center",
+        border: "3px solid green",
+        width: "80%",
+        margin: "30px auto"
+    }
+
+    return (
+        <div style={div}>
+            <h1>{post.name} @ {post.time}</h1>
+            <h2>{post.details}</h2>
+            <button onClick={(event) => edit(post)}>Edit</button>
+            <button onClick={(event) => deleteTodo(post)}>Delete</button>
+            <Link to="/">
+                <button>go back</button>
+            </Link>
+        </div>
+    )
+}
+
+export default SinglePost
